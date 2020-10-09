456caa3fbf32b1215458a4df0432d25893df09f7
Huang-zh <huangzhenghao831@163.com>
2020-10-09 16:21:55 +0800

    不知道修改了什么

--- akuma-mybatis/src/test/java/SimpleSqlSessionFactoryTestDemo.java
+++ akuma-mybatis/src/test/java/SimpleSqlSessionFactoryTestDemo.java
@@ -16,7 +16,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.net.URI;
 import java.sql.ResultSet;
